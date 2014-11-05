16aa0d48423e58ca9a39615aae2cac5a3fb89852
shaun <shaun_hall@outlook.com>
2014-11-04 16:44:25 -0800

    drag n drop

--- app/assets/javascripts/authoring.js
+++ app/assets/javascripts/authoring.js
@@ -5,6 +5,29 @@ var ready = function() {
 
 	$('textarea').autosize();
 
+	$( '#sortable' ).sortable({
+		revert:300,
+		scrollSensitivity: 10,
+		scrollSpeed: 40,
+		//placeholder: "sortable-placeholder",
+		opacity: 0.8,
+		tolerance: "pointer",
+		cursor: "row-resize",
+	});
+
+	$(window).scroll(function(){
+        var posFromTop = $(window).scrollTop();
+
+        if(posFromTop < 200){
+        // if more than 200px from the top do something
+        	$(this).fadeOut(300);
+
+        } else {
+        // otherwise do something else.
+
+        }
+    });
+
 };
 
 $(document).ready(ready);
